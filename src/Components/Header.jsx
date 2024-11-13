7afa520caba4ab1f19e556b11894109f6ca3dfe8
Sabuj-Chowdhury <csabuj73@gmail.com>
2024-11-14 02:01:44 +0530

    'Moment.js' package added to show time

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,15 @@
+import moment from "moment";
 import logo from "../assets/logo.png";
 
 const Header = () => {
   return (
-    <div className="flex flex-col items-center py-5">
+    <div className="flex flex-col items-center gap-3 py-5">
       {/* Logo */}
       <div>
         <img className="w-96" src={logo} alt="" />
       </div>
       <p className="text-gray-400">Journalism without Fear or Favour </p>
+      <p>{moment().format("dddd, MMMM Do YYYY, h:mm a")}</p>
     </div>
   );
 };

1a84986816463a53af4c30f2a9842674c25432f0
Sabuj-Chowdhury <csabuj73@gmail.com>
2024-11-14 02:08:01 +0530

    Readme.md add

---
+++ src/Components/LatestNews.jsx
@@ -0,0 +1,13 @@
+
+const LatestNews = () => {
+    return (
+        <div className="w-11/12 mx-auto">
+            <div>
+                <button></button>
+            </div>
+            
+        </div>
+    );
+};
+
+export default LatestNews;

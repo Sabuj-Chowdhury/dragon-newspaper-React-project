f11a67f4f4f7d0f0a41762479f425f9a11eaaa44
Sabuj-Chowdhury <csabuj73@gmail.com>
2024-11-14 02:26:56 +0530

    React-Fast-Marque npm package add to implement latest

--- src/Components/LatestNews.jsx
+++ src/Components/LatestNews.jsx
@@ -1,14 +1,26 @@
+import Marquee from "react-fast-marquee";
+import { Link } from "react-router-dom";
+
 const LatestNews = () => {
   return (
     <div className="w-11/12 mx-auto">
-      <div className="flex gap-2 justify-center items-center">
+      <div className="flex gap-2 justify-center items-center bg-base-200 p-2">
         <button className="px-3 py-2 bg-[#D72050] text-white">Latest</button>
 
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa
-          perspiciatis architecto, animi totam cumque voluptatem deserunt,
-          molestiae sed perferendis ex nisi.
-        </p>
+        <Marquee pauseOnHover={true} speed={50} className="space-x-10">
+          <Link>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo
+            pariatur fuga iure? .
+          </Link>
+          <Link>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo
+            pariatur fuga iure?
+          </Link>
+          <Link>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo
+            pariatur fuga iure?
+          </Link>
+        </Marquee>
       </div>
     </div>
   );
